ae9d3bbb8616fcd586c45a742724265d86ce084f
Notelseit.com <theprincy@users.noreply.github.com>
2023-01-27 11:56:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 /*
-Plugin Name: Import Novaengel
-Description: Import product and price from Novaengel
-
+Plugin Name: Esegui Script
+Description: Questo plugin consente di eseguire uno script PHP specificato.
+Version: 1.0
+Author: Notelseit Srls
 */
 
 // Assicurati che l'utente abbia i permessi necessari per accedere alla pagina di amministrazione
@@ -13,18 +14,37 @@ if ( ! current_user_can( 'manage_options' ) ) {
 // Variabile per controllare se l'operazione è stata interrotta
 $stop_operation = false;
 
+add_action( 'admin_init', 'script_runner_check_file_permissions' );
+
+// Funzione per verificare che il file script.php non sia stato modificato da un utente malintenzionato
+function script_runner_verify_script_integrity() {
+    $script_file = SCRIPT_RUNNER_DIR . 'novaengels/script.php';
+    $script_file_hash = sha1_file( $script_file );
+    if( $script_file_hash !== 'a3b4c5d6e7f8g9h0i1j2k3l4m5n6o7p8q9r0s1t2u3v4w5x6y7z8' ) {
+        die( 'Il file script.php sembra essere stato modificato. Controllare l\'integrità del file.' );
+    }
+}
+add_action( 'admin_init', 'script_runner_verify_script_integrity' );
 // Verifica se il form è stato inviato
 if ( isset( $_POST['start_stop'] ) ) {
     // Verifica che il valore inviato sia valido
     if ( $_POST['start_stop'] === 'start' ) {
         $stop_operation = false;
+
+        // Verifica che la funzione exec() sia abilitata sul server
+        if ( !function_exists( 'exec' ) ) {
+            wp_die( 'La funzione exec() non è abilitata sul server.' );
+        }
+
+
         // Verifica che il file script.php esista
+
         if ( file_exists( plugin_dir_path( __FILE__ ) . 'script.php' ) ) {
             // Includi il file script.php
             include plugin_dir_path( __FILE__ ) . 'script.php';
             echo '<div id="message" class="updated">Operazione avviata con successo.</div>';
         } else {
-            echo '<div id="message" class="error">Il file script.php non esiste.</div>';
+            echo '<div id="message" class="error">Il file di importazione non è stato trovato</div>';
         }
     } elseif ( $_POST['start_stop'] === 'stop' ) {
         $stop_operation = true;
@@ -56,9 +76,16 @@ function show_script_output() {
         echo '<div id="script-output">' . $output_from_script . '</div>';
     }
 }
+// Funzione per registrare gli errori che possono verificarsi durante l'esecuzione dello script
+function script_runner_log_errors() {
+    ini_set( 'log_errors', 1 );
+    ini_set( 'error_log', SCRIPT_RUNNER_DIR . 'script-errors.log' );
+}
+add_action( 'admin_init', 'script_runner_log_errors' );
 // Crea la pagina di amministrazione per il plugin
 function execute_script_page() {
-    add_menu_page( 'Esegui Script', 'Esegui Script', 'manage_options', 'execute_script', 'execute_script_content', '', 6 );
+    add_menu_page( 'Esegui Script', 'Esegui Script', 'manage_options', 'execute_script', 'execute_script_content', 'script_runner',
+        'script_runner_page_callback', 'dashicons-admin-tools', , '', 6 );
 }
 add_action( 'admin_menu', 'execute_script_page' );
 
